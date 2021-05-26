3ce06a91a7b756a8f433a1479328f77bb1bc24f6
Patrizia Rohr <80752578+galleriesofdreams@users.noreply.github.com>
2021-05-26 15:27:33 +0100

    Dependencies installed

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: './src/client/index.js',
+    mode: 'development',
+    devtool: 'source-map',
+    stats: 'verbose',
+    output: {
+        libraryTarget: 'var',
+        library: 'Client'
+            },
+    module: {
+        rules: [
+            {
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+            },
+            {
+              test: /\.css$/i,
+              use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        }),
+        new CleanWebpackPlugin({
+            dry: true,
+            verbose: true,
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
+        }),
+    ]
+}
---
+++ webpack.prod.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const webpack = require("webpack");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: './src/client/index.js',
+    mode: 'production',
+    output: {
+        libraryTarget: 'var',
+        library: 'Client'
+            },
+    module: {
+        rules: [
+            {
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
+            }
+        ]
+    },
+    plugins:  [
+        new MiniCssExtractPlugin({ filename: "[name].css" }),
+    ]   
+}
