132048cc90546c7fa4cfa7ba3ee1e778a1d59a7d
Patrizia <patrizia.rohr@hotmail.com>
2021-05-28 12:13:10 +0100

    Extra changes as per review

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,7 @@
 import { generateWeather } from './js/app'
 
 
-import './styles/style.scss'
+import '../client/styles/style.scss'
 
 // Event listener to add function to existing HTML DOM element
 document.getElementById('generate').addEventListener('click', generateWeather);
