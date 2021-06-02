5032c29768ec5e9e9065c4b92bf504b73178072c
Patrizia <patrizia.rohr@hotmail.com>
2021-06-02 19:23:50 +0100

    Push

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -4,13 +4,13 @@ const baseURL = 'http://api.geonames.org/searchJSON?';
 /* Function called by event listener */
 export function generateCoords(e){
     const city = document.getElementById('city').value;
-    const latitude = document.getElementById('latitude').value;
-    const longitude = document.getElementById('longitude').value;
+    const arrival = document.getElementById('arrival').value;
+    const departure = document.getElementById('departure').value;
     const GEONAMES_API_KEY = 'galleriesofdreams';
     getCoords (baseURL, GEONAMES_API_KEY, city)
-    .then (function (city) {
+    .then (function (data) {
         postData('http://localhost:3000/addCoords', {
-            lat: latitude, long: longitude, city: city
+            lat: data.latitude, long: data.longitude, city: data.city
             })
             .then(() => {
                 updateUI()
@@ -68,10 +68,10 @@ const getData = async (url='') =>{
 export const updateUI = async () => {
     const request = await fetch('http://localhost:3000/getData');
     try{
-        const lastEntry = await request.json();
-        document.getElementById('latitude').innerHTML = lastEntry["latitude"];
-        document.getElementById('longitude').innerHTML = lastEntry["longitude"];
-        document.getElementById('city').innerHTML = lastEntry["city"];
+        const allData = await request.json();
+        document.getElementById('latitude').innerHTML = allData["latitude"];
+        document.getElementById('longitude').innerHTML = allData["longitude"];
+        document.getElementById('city').innerHTML = allData["city"];
         } catch(error){
             console.log('error', error);
     }
--- src/server/server.js
+++ src/server/server.js
@@ -1,42 +1,25 @@
-
-
-
 // Setup empty JS object to act as endpoint for all routes
 let projectData = {};
 
-// Require Express to run server and routes
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
-// Start up an instance of app
 const app = express();
 
-/* Dependencies */
-const bodyParser = require('body-parser');
-const dotenv = require('dotenv');
-dotenv.config();
-
-
-/* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Cors for cross origin allowance
-const cors = require('cors');
 app.use(cors());
 
-// Initialize the main project folder
 app.use(express.static('dist'));
-const port = 3000;
 
-// Spin up the server
+const port = 3000;
 const server = app.listen(port, () => {
     console.log('server running'); 
     console.log(`running on localhost: ${port}`);
 })
-// Callback to debug
 
-// Initialize all route with a callback function
 // Callback function to complete GET '/all'
 app.get('/getData', (req, res) => {
     console.log('Retrieve projectData');
@@ -44,8 +27,6 @@ app.get('/getData', (req, res) => {
 });
 
 // Post Route
-let data = [];
-
 app.post('/addCoords', (req, res) => {
     console.log(req.body);
     projectData = req.body;
